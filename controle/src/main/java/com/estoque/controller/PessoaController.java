438c968db1b2a268b9d12774f5ecb036be1e153c
OLiUniville <olivandro.sabbi@univille.br>
2024-11-12 22:02:19 -0300

    Ajustes na autenticação
    Realizado ajustes na autenticação da entidade Pessoa.

--- controle/src/main/java/com/estoque/controller/PessoaController.java
+++ controle/src/main/java/com/estoque/controller/PessoaController.java
@@ -1,10 +1,21 @@
 package com.estoque.controller;
 
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.estoque.model.Pessoa;
+import com.estoque.model.Enums.TipoPermissao;
 import com.estoque.service.PessoaService;
 
 @RestController
@@ -15,23 +26,24 @@ public class PessoaController {
     private PessoaService pessoaService;
 
     @GetMapping("/")
-    public List<Pessoa> buscarTodos() {
-        return pessoaService.buscarTodos();
+    public List<Pessoa> buscarTodos(@RequestParam("permissao") TipoPermissao permissao) {
+        return pessoaService.buscarTodos(permissao);
     }
 
     @PostMapping("/")
-    public Pessoa inserir(@RequestBody Pessoa objeto) {
-        return pessoaService.inserirPessoa(objeto);
+    public Pessoa inserir(@RequestBody Pessoa objeto, @RequestParam("permissao") TipoPermissao permissao) {
+        return pessoaService.inserirPessoa(objeto, permissao);
     }
 
     @PutMapping("/")
-    public Pessoa alterar(@RequestBody Pessoa objeto) {
-        return pessoaService.alterarPessoa(objeto);
+    public Pessoa alterar(@RequestBody Pessoa objeto, @RequestParam("permissao") TipoPermissao permissao) {
+        return pessoaService.alterarPessoa(objeto, permissao);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> excluir(@PathVariable("id") Long id) {
-        pessoaService.excluir(id);
+    public ResponseEntity<Void> excluir(@PathVariable("id") Long id, @RequestParam("permissao") TipoPermissao permissao) {
+        pessoaService.excluir(id, permissao);
         return ResponseEntity.ok().build();
     }
 }
+
--- controle/src/main/java/com/estoque/service/PessoaService.java
+++ controle/src/main/java/com/estoque/service/PessoaService.java
@@ -1,10 +1,12 @@
 package com.estoque.service;
-
-import java.util.Date;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.estoque.exception.UnauthorizedException;
 import com.estoque.model.Pessoa;
+import com.estoque.model.Enums.TipoPermissao;
 import com.estoque.repository.PessoaRepository;
 
 @Service
@@ -13,21 +15,31 @@ public class PessoaService {
     @Autowired
     private PessoaRepository pessoaRepository;
 
-    public List<Pessoa> buscarTodos() {
+    public List<Pessoa> buscarTodos(TipoPermissao permissao) {
+        if (!permissao.buscarTodos()) {
+            throw new UnauthorizedException("Permissão negada para buscar registros.");
+        }
         return pessoaRepository.findAll();
     }
 
-    public Pessoa inserirPessoa(Pessoa objeto) {
-        objeto.setDataCriacaoPessoa(new Date());
+    public Pessoa inserirPessoa(Pessoa objeto, TipoPermissao permissao) {
+        if (!permissao.inserir()) {
+            throw new UnauthorizedException("Permissão negada para inserir registros.");
+        }
         return pessoaRepository.saveAndFlush(objeto);
     }
 
-    public Pessoa alterarPessoa(Pessoa objeto) {
-        objeto.setDataAtualizacaoPessoa(new Date());
+    public Pessoa alterarPessoa(Pessoa objeto, TipoPermissao permissao) {
+        if (!permissao.alterar()) {
+            throw new UnauthorizedException("Permissão negada para alterar registros.");
+        }
         return pessoaRepository.saveAndFlush(objeto);
     }
 
-    public void excluir(Long id) {
+    public void excluir(Long id, TipoPermissao permissao) {
+        if (!permissao.excluir()) {
+            throw new UnauthorizedException("Permissão negada para excluir registros.");
+        }
         Pessoa objeto = pessoaRepository.findById(id).orElse(null);
         if (objeto != null) {
             pessoaRepository.delete(objeto);
